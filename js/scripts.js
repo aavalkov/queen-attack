8d0b6c4f46edf91e8d71c59a27d0d202b4e4a99f
Anna Valkov <anna.a.valkov@gmail.com>
2014-07-14 18:34:19 -0700

    specs all pass and finished the JS for queen attack

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var queenAttack = function(initalposition, endposition){
 
-	if (initalposition[1] === endposition[1]){
+	if ((Math.abs(endposition[0] - initalposition[0])) === (Math.abs(endposition[1] - initalposition[1]))){
+		return true;
+	} else if (initalposition[1] === endposition[1]){
 		return true;
 	} else if (initalposition[0] === endposition[0]){
 		return true;
--- specs/specs.js
+++ specs/specs.js
@@ -7,5 +7,8 @@ describe('queenAttack', function() {
   });
    it('is true if the coordinates are vertically in line with each other', function(){
    	queenAttack([4, 6], [4, 8]).should.equal(true);
-   })
+  });
+   it('is true if the coordinates are diagonally in line with each other', function(){
+   	queenAttack([2, 2], [5,5]).should.equal (true);
+   });
 });
